d63dccd846d8ce11e50af57c09e879c025b9cecd
choi ji won <w2019@e-mirim.hs.kr>
2021-04-06 19:27:26 +0900

    예제 4-18 Java로 위젯 속성 변경

--- app/src/main/java/kr/hs/emirim/w2019/chapter4_ex2/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2019/chapter4_ex2/MainActivity.java
@@ -2,13 +2,27 @@ package kr.hs.emirim.w2019.chapter4_ex2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.solution4_2);
+        setContentView(R.layout.ex4_17);
+
+        TextView tv1 = findViewById(R.id.text1);
+        TextView tv2 = findViewById(R.id.text2);
+        TextView tv3 = findViewById(R.id.text3);
+
+        tv1.setText(R.string.hello);
+        tv1.setTextColor(Color.rgb(255,0,0));
+        tv2.setTextSize(30);
+        tv2.setTypeface(Typeface.SANS_SERIF, Typeface.BOLD_ITALIC);
+        tv3.setText(R.string.t5);
+        tv3.setSingleLine();
     }
 }
